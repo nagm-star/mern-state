cb11f513fe48461b3a475ce616b8a658d0331941
nagm-star <nagmo90@gmail.com>
2023-10-02 01:38:26 +0400

    create test api route

--- backend/index.js
+++ backend/index.js
@@ -1,12 +1,12 @@
 import express from 'express';
-import { connectDb } from './config/dbConnection.js';
-import dotenv from 'dotenv';
+// const dotenv = require('dotenv');
 import mongoose from 'mongoose';
-
+import userRouter from './routes/user.route.js';
+// const userRouter  = require('./routes/userRoutes');
 // connectDb();
 
 mongoose
-  .connect(process.env.CONNECTION_STRING, {
+  .connect("mongodb+srv://mgsydqNn7WIYYMzM:mgsydqNn7WIYYMzM@cluster0.2sdkg1v.mongodb.net/contacts-backend?retryWrites=true&w=majority", {
     useNewUrlParser: true,
     useUnifiedTopology: true
   })
@@ -21,9 +21,19 @@ mongoose
 
 const app = express();
 
+
 const port = process.env.PORT || 5000;
+
 
 
 app.listen(port, () => {
     console.log('Server is running on port 5000');
 })
+
+
+app.use("/api/user", userRouter);
+// app.get("/test", (req, res)=> {
+//   res.json({
+//       message: 'Hello world!',
+//   });
+// });
